21dcce6c19fd02eafbc008e6167752aba367346b
jh <cipher.keys@gmail.com>
2024-05-27 19:19:24 +0200

    modify

--- module_02/train.py
+++ module_02/train.py
@@ -1,9 +1,11 @@
 import os
 import pickle
+
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -22,12 +24,18 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.sklearn.autolog()
 
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 if __name__ == '__main__':
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("homework-02")
+
     run_train()
